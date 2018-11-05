7544b92d91ea77a70dbd136e84be89466360d667
Mario Velyov <mario.velyov@accedia.com>
2018-11-05 12:06:52 +0200

    added router links to home, users and repos components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ReposComponent } from './repos/repos.component';
 import { UsersComponent } from './users/users.component';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'repositories', component: ReposComponent },
 ];
